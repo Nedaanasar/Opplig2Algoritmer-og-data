e50eb7a33fe58d25edc201afdb172b16892c6261
Amr <amr.hakmi@hotmail.com>
2020-10-07 13:59:47 +0200

    prøve i main metode oppgavene 3,8.

--- src/no/oslomet/cs/algdat/main.java
+++ src/no/oslomet/cs/algdat/main.java
@@ -17,7 +17,18 @@ public class main {
         System.out.println("legg nummer 5 til liste {0,1,2,3,4} i bakerst" + liste.toString());
         liste.leggInn(3, 88);
         System.out.println("legg nummer 88 til liste {0,1,2,3,4,5} i midten " + liste.toString());
-
-
+        //sjekke oppgave 3
+        Character[] c = {'A','B','C','D','E','F','G','H','I','J',};
+        DobbeltLenketListe<Character> liste2 =new DobbeltLenketListe<>(c);
+        System.out.println(liste2.subliste(3,8));
+        System.out.println(liste2.subliste(5,5));
+        System.out.println(liste2.subliste(8,liste2.antall()));
+        //System.out.println(liste2.subliste(0,11));
+        // prøve oppgave 8
+        String[] navn = {"Lars","Anders","Bodil","Kari","Per","Berit"};
+        Liste<String> liste3 =new DobbeltLenketListe<>(navn);
+        liste.forEach(s -> System.out.print(s + "​ ​"));
+        System.out.println();
+         for(String s :liste3) System.out.print(s +"​ ​");
     }
 }
